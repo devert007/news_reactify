2bb240abf4d96e2843953919033d31f61e902761
devert007 <kislydevert007@gmail.com>
2024-10-31 13:58:34 +0300

    fixes and add scrolling

--- src/components/NewsFilters/NewsFilters.jsx
+++ src/components/NewsFilters/NewsFilters.jsx
@@ -1,29 +1,32 @@
 import { getCategories } from "../../api/apiNews";
 import { useFetch } from "../../helpers/hooks/useFetch";
 import Categories from "../Categories/Categories";
-import Seacrh from "../Search/Search";
+import Search from "../Search/Search";
+import Slider from "../Slider/Slider";
 import styles from "./styles.module.css";
-const NewsFilters = ({filters,changeFilter}) => {
-	const { data: dataCategories } = useFetch(getCategories);
 
-	return (
-		<div className={styles.filters}>
-			{dataCategories ? (
-				<Categories
-					categories={dataCategories.categories}
-					setSelectedCategory={filters.category}
-					selectedCategory={(category) => {
-						changeFilter("category", category);
-					}}
-				/>
-			) : null}
-			<Seacrh
-				keywords={filters.keywords}
-				setKeywords={(keywords) => {
-					changeFilter("keywords", keywords);
-				}}
-			/>
-		</div>
-	);
+const NewsFilters = ({ filters, changeFilter }) => {
+  const { data: dataCategories } = useFetch(getCategories);
+  return (
+    <div className={styles.filters}>
+      {dataCategories ? (
+        <Slider step={340}>
+          <Categories
+            categories={dataCategories.categories}
+            selectedCategory={filters.category}
+            setSelectedCategory={(category) =>
+              changeFilter("category", category)
+            }
+          />
+        </Slider>
+      ) : null}
+
+      <Search
+        keywords={filters.keywords}
+        setKeywords={(keywords) => changeFilter("keywords", keywords)}
+      />
+    </div>
+  );
 };
+
 export default NewsFilters;
