625ded9f3c09fc0d25b7485398cbf076515fec57
devert007 <kislydevert007@gmail.com>
2024-11-01 11:03:05 +0300

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from "react";
 import Header from "./components/Header/Header";
 import Main from "./pages/Main/Main";
 
 function App() {
-	const [count, setCount] = useState(0);
 
 	return (
 		<>
